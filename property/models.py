f72376bbca64badfe45b2a8d121a158cfd4911b1
Lleanin <Fifashf@yandex.ru>
2024-11-17 00:33:04 +0100

    setup filtering in admin UI.

--- property/models.py
+++ property/models.py
@@ -6,7 +6,7 @@ class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField(blank=True, null=True)
-    
+
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
